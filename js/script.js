a9c30b69a943a196e4b22311b55bf24af7fa767c
KazJunior <kazuo.spc@gmail.com>
2026-03-26 21:30:16 -0300

    menu hamburg

--- js/script.js
+++ js/script.js
@@ -2,9 +2,16 @@
 
 document.addEventListener('DOMContentLoaded' , () => {
     
+    //Var Global
     //objeto no html (sol lua)
     const toggleTheme = document.getElementById('toggleTheme');
     const rootHtml = document.documentElement;
+    // Var para menu Hamburguer
+    const menuHamburger = document.getElementById('menuHamburger');
+    const menuMobile = document.getElementById('menuMobile');
+
+    const icon = menuHamburger?.querySelector('i');
+
 
     //Função mudar a cor do tema (dark / light)
 
@@ -24,4 +31,18 @@ document.addEventListener('DOMContentLoaded' , () => {
         toggleTheme.addEventListener('click', changeTheme);
     }
 
+    //Alternar menu mobile e icone
+    function toggleMenu() {
+        const isOpen = menuMobile.classList.toggle('active');
+        //condicao
+
+        if(icon) {
+            icon.classList.toggle('bi-list', !isOpen);
+            icon.classList.toggle('bi-x-lg', isOpen);
+        }
+    }
+
+    if (menuHamburger) {
+        menuHamburger.addEventListener('click', toggleMenu);    
+    }
 });
